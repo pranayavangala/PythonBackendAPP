15623cb32778e5df99174975a67e8eeb47c92c63
Pranaya <pranayavanagala@gmail.com>
2026-06-11 13:03:45 -0500

    Switch to SQLite for Render deployment

--- app/database.py
+++ app/database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from sqlalchemy.orm import declarative_base
+DATABASE_URL = "sqlite:///./crmdb.db"
 
-DATABASE_URL = (
-    "mysql+pymysql://root:root@localhost:3306/crmdb"
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
-
-engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
