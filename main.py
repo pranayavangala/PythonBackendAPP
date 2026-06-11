a4a919d1f1f1b3b3855b294fb64628edbbcbcfa8
Pranaya <pranayavanagala@gmail.com>
2026-06-11 13:26:38 -0500

    Fix CORS and main.py

--- main.py
+++ main.py
@@ -8,8 +8,17 @@ from app.routes import router
 
 app = FastAPI(
     title="Python Backend App",
-    description="FastAPI + MySQL + SQLAlchemy project",
+    description="FastAPI + SQLAlchemy project",
     version="1.0.0",
+)
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Serve uploaded images
@@ -17,18 +26,6 @@ app.mount(
     "/uploads",
     StaticFiles(directory="uploads"),
     name="uploads",
-)
-
-# CORS Configuration
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://shop-flowers-here.vercel.app/",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 Base.metadata.create_all(bind=engine)
