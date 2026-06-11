178fe78d20b6041b4e85e4b1cf335242bca500d2
Pranaya <pranayavanagala@gmail.com>
2026-06-11 13:07:17 -0500

    Fix deployment

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DECIMAL
 from app.database import Base
 
+
 class User(Base):
     __tablename__ = "users1"
 
@@ -9,11 +10,11 @@ class User(Base):
     email = Column(String(255), unique=True, index=True)
     password = Column(String(255))
 
+
 class Flower(Base):
     __tablename__ = "flowers"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100))
-    price = Column(DECIMAL(10,2))
+    price = Column(DECIMAL(10, 2))
     image = Column(String(255))
-
--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://YOUR_PROJECT.vercel.app",
+        "https://nirnayaflorals.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
